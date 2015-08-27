0ed1c0d141d15e26408e43ed653037194ad5ef80
UnRealDinnerbone <tsurles357@gmail.com>
2015-08-27 18:06:02 -0500

    Removed Yarm Compact

--- src/main/java/com/unrealdinnerbone/yarm/Compact/Test.java
+++
@@ -1,9 +0,0 @@
-package com.unrealdinnerbone.yarm.Compact;
-
-import com.unrealdinnerbone.yarm.init.ModItems;
-import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.item.ItemStack;
-
-public class Test {
-
-}
