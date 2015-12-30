f88e3a74b5019fb07b3960edd73b58335c992ebf
Unreal Dinnerbone <unrealdinnerbone@gmail.com>
2015-12-30 15:29:47 -0600

    Update Version
    I DID THIS TIME

--- src/main/java/com/unrealdinnerbone/yarm/reference/Reference.java
+++ src/main/java/com/unrealdinnerbone/yarm/reference/Reference.java
@@ -5,7 +5,7 @@ public class Reference
 {
     public static final String MOD_ID = "yarm";
     public static final String MOD_NAME = "Yet Another Random Mod - YARM";
-    public static final String VERSION = "1.6.1";
+    public static final String VERSION = "2.0.0";
     public static final String SERVER_SIDE = "com.unrealdinnerbone.yarm.proxy.ServerProxy";
     public static final String CLIENT_SIDE = "com.unrealdinnerbone.yarm.proxy.ClientProxy";
     public static final String RE_PREFIX = MOD_ID.toLowerCase() + ":";
