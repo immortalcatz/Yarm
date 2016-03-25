8a9b672e48405f00a7eadb710b5b5d14c4b5fa35
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2016-03-24 22:42:20 -0500

    Bacon
    B

--- src/main/java/com/unrealdinnerbone/yarm/client/gui/BlockGUI/BlockGUIMain.java
+++ src/main/java/com/unrealdinnerbone/yarm/client/gui/BlockGUI/BlockGUIMain.java
@@ -1,5 +1,6 @@
 package com.unrealdinnerbone.yarm.client.gui.BlockGUI;
 
+import com.unrealdinnerbone.yarm.Util.LogHelper;
 import com.unrealdinnerbone.yarm.Util.Reference;
 import com.unrealdinnerbone.yarm.init.ModItems;
 import net.minecraft.client.Minecraft;
@@ -12,16 +13,11 @@ import java.io.IOException;
 
 public class BlockGUIMain extends GuiScreen
 {
-    private final int guiWidth = 237;
+    private final int guiWidth = 233;
     private final int guiHeight = 234;
 
-    private final int tabWidth = 8;
-    private final int tabHeight = 277;
-
-    private final int ButtonWidth = 20;
-    private final int ButtonHeight = 20;
-
-    private GuiButton selectedButton;
+    private final int ButtonWidth = 50;
+    private final int ButtonHeight = 50;
 
     private GuiButton Main;
     private GuiButton Test;
@@ -41,19 +37,19 @@ public class BlockGUIMain extends GuiScreen
 
         mc.renderEngine.bindTexture(new ResourceLocation(Reference.MOD_ID, "textures/gui/test/test.png"));
         drawTexturedModalRect(x, y, 0, 0 , guiWidth, guiHeight);
+        drawTexturedModalRect(x - 30 , y, 260, 4, ButtonWidth, ButtonHeight);
 //        fontRendererObj.drawString(String.valueOf(Minecraft.getDebugFPS()), x + 10, y + 10, 0x404040);
 //        fontRendererObj.drawString(Minecraft.getMinecraft().getSession().getUsername(), x + 10, y + 20 , 0x99FF00);
-        mc.getRenderItem().renderItemIntoGUI(new ItemStack(ModItems.Item_ManmedFace), x , y);
+        mc.getRenderItem().renderItemIntoGUI(new ItemStack(ModItems.Item_ManmedFace), x + 20 , y + 30);
 
     }
 
     @Override
-    public void initGui() {
-        int guiWidth2 = guiWidth / 2;
-        int guiHeight2 = guiHeight / 2;
-
-        this.buttonList.add(Main = new GuiButton(0, guiWidth2 - 44, guiHeight2 - 80, 20, 20, ""));
-        this.buttonList.add(Test = new GuiButton(1, guiWidth2 - 44, guiHeight2 - 100, 20, 20, ""));
+    public void initGui()
+    {
+        int x = (width - guiWidth) / 2;
+        int y = (height - guiHeight) / 2;
+        LogHelper.info("Width: " + width);
     }
     @Override
     public boolean doesGuiPauseGame()
--- src/main/java/com/unrealdinnerbone/yarm/client/gui/BlockGUI/BlockGUITest.java
+++ src/main/java/com/unrealdinnerbone/yarm/client/gui/BlockGUI/BlockGUITest.java
@@ -12,16 +12,8 @@ import java.io.IOException;
 
 public class BlockGUITest extends GuiScreen
 {
-    private final int guiWidth = 237;
+    private final int guiWidth = 234;
     private final int guiHeight = 234;
-
-    private final int tabWidth = 8;
-    private final int tabHeight = 277;
-
-    private final int ButtonWidth = 20;
-    private final int ButtonHeight = 20;
-
-    private GuiButton selectedButton;
 
     private GuiButton Main;
     private GuiButton Test;
@@ -43,17 +35,20 @@ public class BlockGUITest extends GuiScreen
         drawTexturedModalRect(x, y, 0, 0 , guiWidth, guiHeight);
 //        fontRendererObj.drawString(String.valueOf(Minecraft.getDebugFPS()), x + 10, y + 10, 0x404040);
 //        fontRendererObj.drawString(Minecraft.getMinecraft().getSession().getUsername(), x + 10, y + 20 , 0x99FF00);
-        mc.getRenderItem().renderItemIntoGUI(new ItemStack(ModItems.Item_UnRealFace), x , y);
+        mc.getRenderItem().renderItemIntoGUI(new ItemStack(ModItems.Item_UnRealFace), x + 20 , y + 30);
 
     }
 
     @Override
     public void initGui() {
+        int x = (width - guiWidth) / 2;
+        int y = (height - guiHeight) / 2;
+
         int guiWidth2 = guiWidth / 2;
         int guiHeight2 = guiHeight / 2;
 
-        this.buttonList.add(Main = new GuiButton(0, guiWidth2 - 44, guiHeight2 - 80, 20, 20, ""));
-        this.buttonList.add(Test = new GuiButton(1, guiWidth2 - 44, guiHeight2 - 100, 20, 20, ""));
+        this.buttonList.add(Main = new GuiButton(0, x - 44, x - 80, 20, 20, "Main"));
+        this.buttonList.add(Test = new GuiButton(1, y - 44, y - 60, 20, 20, "Test"));
     }
     @Override
     public boolean doesGuiPauseGame()
