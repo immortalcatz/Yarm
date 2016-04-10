3e8bf9251427ea5090d5f620f39336cfcee96098
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2016-04-10 16:59:02 -0500

    Fixed The Mod Update Thing

--- src/main/java/com/unrealdinnerbone/yarm/Util/Reference.java
+++ src/main/java/com/unrealdinnerbone/yarm/Util/Reference.java
@@ -5,12 +5,12 @@ public class Reference
 {
     public static final String MOD_ID = "yarm";
     public static final String MOD_NAME = "Yet Another Random Mod - YARM";
-    public static final String VERSION = "@VERSION@";
+    public static final String VERSION = "3.3.0";
     public static final String MC_VERSION = "1.8.9";
     public static final String SERVER_SIDE = "com.unrealdinnerbone.yarm.proxy.ServerProxy";
     public static final String CLIENT_SIDE = "com.unrealdinnerbone.yarm.proxy.ClientProxy";
     public static final String GUI_FACTORY_CLASS = "com.unrealdinnerbone.yarm.client.gui.GuiFactory";
-    public static final String MOD_UPDATE_JSON_URL = "http://mc.unrealdinnerbone.x10.bz/yarm/Updater.json";
+    public static final String MOD_UPDATE_JSON_URL = "http://mc.unrealdinnerbone.x10.bz/Yarm/Updater.json";
 
     public static final String RE_PREFIX = MOD_ID.toLowerCase() + ":";
     public static final String MOD_DISPLAY_NAME_WITH_VERSION = MOD_ID.toUpperCase() + VERSION;
