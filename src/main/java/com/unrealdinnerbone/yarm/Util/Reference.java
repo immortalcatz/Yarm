a71b91f0f18cffac8f7f499138d1c1a7aaaad66f
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2016-04-12 19:56:19 -0500

    beee
    beee

--- src/main/java/com/unrealdinnerbone/yarm/Util/Reference.java
+++ src/main/java/com/unrealdinnerbone/yarm/Util/Reference.java
@@ -5,7 +5,7 @@ public class Reference
 {
     public static final String MOD_ID = "yarm";
     public static final String MOD_NAME = "Yet Another Random Mod - YARM";
-    public static final String VERSION = "3.3.0";
+    public static final String VERSION = "3.4.0";
     public static final String MC_VERSION = "1.8.9";
     public static final String SERVER_SIDE = "com.unrealdinnerbone.yarm.proxy.ServerProxy";
     public static final String CLIENT_SIDE = "com.unrealdinnerbone.yarm.proxy.ClientProxy";
