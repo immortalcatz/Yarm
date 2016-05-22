64169169e08faae5d35175888d09c4d5c2a6e1b9
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2016-05-21 21:53:37 -0500

    001
    001

--- src/main/java/com/unrealdinnerbone/yarm/init/ModBlocks.java
+++ src/main/java/com/unrealdinnerbone/yarm/init/ModBlocks.java
@@ -14,23 +14,8 @@ public class ModBlocks
 {
     //Thiangs
     public static final BlockUnRealFace BLOCK_UNREAL_FACE = new BlockUnRealFace();
-    //Ores
-    public static final BlockAntimonyOre BLOCK_ANTIMONY_ORE = new BlockAntimonyOre();
-    public static final BlockBismuthOre BLOCK_BISMUTH_ORE = new BlockBismuthOre();
-    public static final BlockChromiumOre BLOCK_CHROMIUM_ORE = new BlockChromiumOre();
-    public static final BlockCobaltOre BLOCK_COBALT_ORE = new BlockCobaltOre();
-    public static final BlockCopperOre BLOCK_COPPER_ORE = new BlockCopperOre();
-    public static final BlockGalliumOre BLOCK_GALLIUM_ORE = new BlockGalliumOre();
-    public static final BlockIndiumOre BLOCK_INDIUM_ORE = new BlockIndiumOre();
-    public static final BlockLeadOre BLOCK_LEAD_ORE = new BlockLeadOre();
-    public static final BlockMagnesiumOre BLOCK_MAGNESIUM_ORE = new BlockMagnesiumOre();
-    public static final BlockNickelOre BLOCK_NICKEL_ORE = new BlockNickelOre();
-    public static final BlockSilverOre BLOCK_SILVER_ORE = new BlockSilverOre();
-    public static final BlockTinOre BLOCK_TIN_ORE = new BlockTinOre();
-    public static final BlockTitaniumOre BLOCK_TITANIUM_ORE = new BlockTitaniumOre();
-    public static final BlockZincOre BLOCK_ZINC_ORE = new BlockZincOre();
-    public static final BlockZirconiumOre BLOCK_ZIRCONIUM_ORE = new BlockZirconiumOre();
     //OreBlocks
+    public static final BlockAntimonyOre BLOCK_ANTIMONY_ORE = new BlockAntimonyOre();
     public static final BlockAntimony BLOCK_ANTIMONY = new BlockAntimony();
     //NetherStarBlcoks
     public static final BlockNetherStar1 BLOCK_NETHER_STAR_1 = new BlockNetherStar1();
@@ -47,20 +32,23 @@ public class ModBlocks
 
     public static void init()
     {
-         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_ZINC_ORE, 0, BlockZincOre.getName(), OreDictionaryNames.BLOCK_ZINC_ORE, ItemStacks.BlockZincOre(1));
-         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_ZIRCONIUM_ORE, 0, BlockZirconiumOre.getName(), OreDictionaryNames.BLOCK_ZIRCONIUM_ORE, ItemStacks.BlockZirconiumOre(1));
-         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_TITANIUM_ORE, 0, BlockTitaniumOre.getName(), OreDictionaryNames.BLOCK_TITANIUM_ORE, ItemStacks.BlockTitaniumOre(1));
-         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_TIN_ORE, 0, BlockTinOre.getName(), OreDictionaryNames.BLOCK_TIN_ORE, ItemStacks.BlockTinOre(1));
-         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_SILVER_ORE, 0, BlockSilverOre.getName(), OreDictionaryNames.BLOCK_SILVER_ORE, ItemStacks.BlockSilverOre(1));
-         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_NICKEL_ORE, 0, BlockNickelOre.getName(), OreDictionaryNames.BLOCK_NICKEL_ORE, ItemStacks.BlockNickelOre(1));
-         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_MAGNESIUM_ORE, 0, BlockMagnesiumOre.getName(), OreDictionaryNames.BLOCK_MAGNESIUM_ORE, ItemStacks.BlockMagnesiumOre(1));
-         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_LEAD_ORE, 0, BlockLeadOre.getName(), OreDictionaryNames.BLOCK_LEAD_ORE, ItemStacks.BlockLeadOre(1));
-         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_INDIUM_ORE, 0, BlockIndiumOre.getName(), OreDictionaryNames.BLOCK_INDIUM_ORE, ItemStacks.BlockIndiumOre(1));
-         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_GALLIUM_ORE, 0, BlockGalliumOre.getName(), OreDictionaryNames.BLOCK_GALLIUM_ORE, ItemStacks.BlockGalliumOre(1));
-         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_COPPER_ORE, 0, BlockCopperOre.getName(), OreDictionaryNames.BLOCK_COPPER_ORE, ItemStacks.BlockCobaltOre(1));
-         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_COBALT_ORE, 0, BlockCobaltOre.getName(), OreDictionaryNames.BLOCK_COBALT_ORE, ItemStacks.BlockCobaltOre(1));
-         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_CHROMIUM_ORE, 0, BlockChromiumOre.getName(), OreDictionaryNames.BLOCK_CHROMIUM_ORE, ItemStacks.BlockChromiumOre(1));
-         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_BISMUTH_ORE, 0, BlockBismuthOre.getName(), OreDictionaryNames.BLOCK_BISMUTH_ORE, ItemStacks.BlockBismuthOre(1));
+
+        //Todo Work on ore to make the mod better Disabled them for now till get a better Config System
+//         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_ZINC_ORE, 0, BlockZincOre.getName(), OreDictionaryNames.BLOCK_ZINC_ORE, ItemStacks.BlockZincOre(1));
+//         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_ZIRCONIUM_ORE, 0, BlockZirconiumOre.getName(), OreDictionaryNames.BLOCK_ZIRCONIUM_ORE, ItemStacks.BlockZirconiumOre(1));
+//         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_TITANIUM_ORE, 0, BlockTitaniumOre.getName(), OreDictionaryNames.BLOCK_TITANIUM_ORE, ItemStacks.BlockTitaniumOre(1));
+//         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_TIN_ORE, 0, BlockTinOre.getName(), OreDictionaryNames.BLOCK_TIN_ORE, ItemStacks.BlockTinOre(1));
+//         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_SILVER_ORE, 0, BlockSilverOre.getName(), OreDictionaryNames.BLOCK_SILVER_ORE, ItemStacks.BlockSilverOre(1));
+//         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_NICKEL_ORE, 0, BlockNickelOre.getName(), OreDictionaryNames.BLOCK_NICKEL_ORE, ItemStacks.BlockNickelOre(1));
+//         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_MAGNESIUM_ORE, 0, BlockMagnesiumOre.getName(), OreDictionaryNames.BLOCK_MAGNESIUM_ORE, ItemStacks.BlockMagnesiumOre(1));
+//         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_LEAD_ORE, 0, BlockLeadOre.getName(), OreDictionaryNames.BLOCK_LEAD_ORE, ItemStacks.BlockLeadOre(1));
+//         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_INDIUM_ORE, 0, BlockIndiumOre.getName(), OreDictionaryNames.BLOCK_INDIUM_ORE, ItemStacks.BlockIndiumOre(1));
+//         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_GALLIUM_ORE, 0, BlockGalliumOre.getName(), OreDictionaryNames.BLOCK_GALLIUM_ORE, ItemStacks.BlockGalliumOre(1));
+//         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_COPPER_ORE, 0, BlockCopperOre.getName(), OreDictionaryNames.BLOCK_COPPER_ORE, ItemStacks.BlockCobaltOre(1));
+//         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_COBALT_ORE, 0, BlockCobaltOre.getName(), OreDictionaryNames.BLOCK_COBALT_ORE, ItemStacks.BlockCobaltOre(1));
+//         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_CHROMIUM_ORE, 0, BlockChromiumOre.getName(), OreDictionaryNames.BLOCK_CHROMIUM_ORE, ItemStacks.BlockChromiumOre(1));
+//         RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_BISMUTH_ORE, 0, BlockBismuthOre.getName(), OreDictionaryNames.BLOCK_BISMUTH_ORE, ItemStacks.BlockBismuthOre(1));
+
          RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_ANTIMONY_ORE, 0, BlockAntimonyOre.getName(), OreDictionaryNames.BLOCK_ANTIMONY_ORE, ItemStacks.BlockAntimonyOre(1));
          RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_UNREAL_FACE, 0, BlockUnRealFace.getName(), OreDictionaryNames.BLOCK_UNREAL_FACE, ItemStacks.BlockUnRealFace(1));
          RegisterHelper.RegisterBlockWithOreDictionary(BLOCK_ANTIMONY, 0, BlockAntimony.getName(), OreDictionaryNames.BLOCK_ANTIMONY, ItemStacks.BlockAntimony(1));
@@ -76,3 +64,18 @@ public class ModBlocks
         LogHelper.info("Loaded Blocks");
     }
 }
+
+//    public static final BlockBismuthOre BLOCK_BISMUTH_ORE = new BlockBismuthOre();
+//    public static final BlockChromiumOre BLOCK_CHROMIUM_ORE = new BlockChromiumOre();
+//    public static final BlockCobaltOre BLOCK_COBALT_ORE = new BlockCobaltOre();
+//    public static final BlockCopperOre BLOCK_COPPER_ORE = new BlockCopperOre();
+//    public static final BlockGalliumOre BLOCK_GALLIUM_ORE = new BlockGalliumOre();
+//    public static final BlockIndiumOre BLOCK_INDIUM_ORE = new BlockIndiumOre();
+//    public static final BlockLeadOre BLOCK_LEAD_ORE = new BlockLeadOre();
+//    public static final BlockNickelOre BLOCK_NICKEL_ORE = new BlockNickelOre();
+//    public static final BlockSilverOre BLOCK_SILVER_ORE = new BlockSilverOre();
+//    public static final BlockTinOre BLOCK_TIN_ORE = new BlockTinOre();
+//    public static final BlockTitaniumOre BLOCK_TITANIUM_ORE = new BlockTitaniumOre();
+//    public static final BlockZincOre BLOCK_ZINC_ORE = new BlockZincOre();
+//    public static final BlockZirconiumOre BLOCK_ZIRCONIUM_ORE = new BlockZirconiumOre();
+//    public static final BlockMagnesiumOre BLOCK_MAGNESIUM_ORE = new BlockMagnesiumOre();
--- src/main/java/com/unrealdinnerbone/yarm/init/ModEntity.java
+++ src/main/java/com/unrealdinnerbone/yarm/init/ModEntity.java
@@ -1,8 +1,8 @@
 package com.unrealdinnerbone.yarm.init;
 
 import com.unrealdinnerbone.yarm.Util.LogHelper;
+import com.unrealdinnerbone.yarm.entitys.entityManmaed;
 import com.unrealdinnerbone.yarm.yarm;
-import net.minecraft.entity.EntityList;
 
 import com.unrealdinnerbone.yarm.entitys.entityUnReal;
 
@@ -13,7 +13,7 @@ public class ModEntity
     public static void initEntity()
     {
         createEntity(entityUnReal.class, "entityUnReal", 0x535353, 0xB8B8B8);
-        createEntity(com.unrealdinnerbone.yarm.entitys.entityManmaed.class, "entityManmaed", 0x535353, 0xB8B8B8);
+        createEntity(entityManmaed.class, "entityManmaed", 0x535353, 0xB8B8B8);
         // How To Regsiter An Enity
         // createEntity(entityclassname, "entityname", firsteggcolor, secondaryeggcolor);
     }
@@ -21,11 +21,8 @@ public class ModEntity
     public static void createEntity(Class entityClass, String entityName, int eggPrimaryColor, int eggSecondaryColor)
     {
         EntityRegistry.registerModEntity(entityClass, entityName, 154154, yarm.instance, 64, 1, true);
-    }
-
-    private static void createEgg(int RandomId, int eggPrimaryColor, int eggSecondaryColor, String EnityName)
-    {
-        //EntityList.entityEggs.put(String.valueOf(Integer.valueOf(RandomId)), new EntityList.EntityEggInfo(EnityName, eggPrimaryColor, eggSecondaryColor));
+        EntityRegistry.registerEgg(entityClass, eggPrimaryColor, eggSecondaryColor);
         LogHelper.info("Loaded Entity's");
     }
+
 }
