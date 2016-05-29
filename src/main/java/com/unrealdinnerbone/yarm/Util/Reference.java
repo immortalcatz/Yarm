7aed57bec4ec600ecd19598eea27521a5e4b49f1
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2016-05-28 22:03:36 -0500

    Bump version numbers for curse beta rellise

--- src/main/java/com/unrealdinnerbone/yarm/Util/Reference.java
+++ src/main/java/com/unrealdinnerbone/yarm/Util/Reference.java
@@ -5,7 +5,7 @@ public class Reference
 {
     public static final String MOD_ID = "yarm";
     public static final String MOD_NAME = "Yet Another Random Mod - YARM";
-    public static final String VERSION = "4.2.0";
+    public static final String VERSION = "4.3.0";
     public static final String MC_VERSION = "1.9.4";
     public static final String SERVER_SIDE = "com.unrealdinnerbone.yarm.proxy.ServerProxy";
     public static final String CLIENT_SIDE = "com.unrealdinnerbone.yarm.proxy.ClientProxy";
