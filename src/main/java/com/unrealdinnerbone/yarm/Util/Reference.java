c0a5c5eea142b2b74179bcc29184acf6bb9cd59f
UnRealDinnerbone <UnRealDinnerbone@users.noreply.github.com>
2016-06-26 12:37:23 -0500

    Addded """

--- src/main/java/com/unrealdinnerbone/yarm/Util/Reference.java
+++ src/main/java/com/unrealdinnerbone/yarm/Util/Reference.java
@@ -6,7 +6,7 @@ public class Reference
     public static final String MOD_ID = "yarm";
     public static final String MOD_NAME = "Yet Another Random Mod - YARM";
     public static final String VERSION = "5.0.0";
-    public static final String MC_VERSION = "1.10;
+    public static final String MC_VERSION = "1.10";
     public static final String SERVER_SIDE = "com.unrealdinnerbone.yarm.proxy.ServerProxy";
     public static final String CLIENT_SIDE = "com.unrealdinnerbone.yarm.proxy.ClientProxy";
     public static final String GUI_FACTORY_CLASS = "com.unrealdinnerbone.yarm.client.gui.GuiFactory";
